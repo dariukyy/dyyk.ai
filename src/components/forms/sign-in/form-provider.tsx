e4c54ca61faf1d3f1c4ffd50e16f62facadb36fa
Darius <dpdarius1@gmail.com>
2024-08-29 17:58:33 +0300

    finishing auth

---
+++ src/components/forms/sign-in/form-provider.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { Loader } from "@/components/loader";
+import { AuthContextProvider } from "@/context/use-auth-context";
+import { useSignInForm } from "@/hooks/sign-in/use-sign-in";
+import { PropsWithChildren } from "react";
+import { FormProvider } from "react-hook-form";
+
+type SignInFormProviderProps = PropsWithChildren;
+
+function SignInFormProvider({ children }: SignInFormProviderProps) {
+  const { methods, onHandleSubmit, loading } = useSignInForm();
+  return (
+    <AuthContextProvider>
+      <FormProvider {...methods}>
+        <form onSubmit={onHandleSubmit} className="h-full">
+          <div className="flex flex-col justify-between gap-3 h-full">
+            <Loader loading={loading}>{children}</Loader>
+          </div>
+        </form>
+      </FormProvider>
+    </AuthContextProvider>
+  );
+}
+
+export default SignInFormProvider;

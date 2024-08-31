c9dcb06a2802ebbee507bb1a607f30e80e196c8a
Darius <dpdarius1@gmail.com>
2024-08-31 23:00:41 +0300

    finishing help-desk component

---
+++ src/components/forms/settings/help-desk.tsx
@@ -0,0 +1,75 @@
+"use client";
+
+import Section from "@/components/section-label";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardTitle,
+} from "@/components/ui/card";
+import { useHelpDesk } from "@/hooks/settings/use-settings";
+import FormGenerator from "../form-generator";
+import { Button } from "@/components/ui/button";
+import { Loader } from "@/components/loader";
+import Accordion from "@/components/accordian";
+
+function HelpDesk({ id }: { id: string }) {
+  const { register, onSubmitQuestion, errors, isQuestions, loading } =
+    useHelpDesk(id);
+  return (
+    <Card className="w-full grid grid-cols-1 lg:grid-cols-2">
+      <CardContent className="p-6 border-r-[-1px]">
+        <CardTitle>Help Desk</CardTitle>
+        <form className="flex flex-col gap-6 mt-10" onSubmit={onSubmitQuestion}>
+          <div className="flex flex-col gap-3">
+            <Section
+              label="Question"
+              message="Add a question that you believe is frequently asked by your users."
+            />
+            <FormGenerator
+              inputType="input"
+              register={register}
+              errors={errors}
+              name="question"
+              type="text"
+              placeholder="Type your question"
+              form="help-desk-form"
+            />
+          </div>
+          <div className="flex flex-col gap-3">
+            <FormGenerator
+              inputType="textarea"
+              lines={5}
+              register={register}
+              name="answer"
+              errors={errors}
+              form="help-desk-form"
+              type="text"
+              placeholder="Type your answer"
+            />
+          </div>
+          <Button className="bg-orange hover:bg-orange/70 transition duration-150 ease-in-out text-white font-semibold">
+            Create Question
+          </Button>
+        </form>
+      </CardContent>
+      <CardContent className="p-6 overflow-y-auto chat-window">
+        <Loader loading={loading}>
+          {isQuestions.length ? (
+            isQuestions.map((question) => (
+              <Accordion
+                key={question.id}
+                trigger={question.question}
+                content={question.answer}
+              />
+            ))
+          ) : (
+            <CardDescription>No Questions to show</CardDescription>
+          )}
+        </Loader>
+      </CardContent>
+    </Card>
+  );
+}
+
+export default HelpDesk;

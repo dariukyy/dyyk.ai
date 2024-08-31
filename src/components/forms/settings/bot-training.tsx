07043a900bbacaad0582fd21a390cf1bda848794
Darius <dpdarius1@gmail.com>
2024-08-31 23:07:07 +0300

    v1 of Domain page

--- src/components/forms/settings/bot-training.tsx
+++ src/components/forms/settings/bot-training.tsx
@@ -1,5 +1,36 @@
-function BotTrainingForm() {
-  return <div></div>;
-}
+import { TabsContent } from "@/components/ui/tabs";
+import { HELP_DESK_TABS_MENU } from "@/constants/menu";
+import React from "react";
+import FilterQuestions from "./filter-questions";
+import TabsMenu from "@/components/tabs";
+import HelpDesk from "./help-desk";
+import { Separator } from "@/components/ui/separator";
+
+type Props = {
+  id: string;
+};
+
+const BotTrainingForm = ({ id }: Props) => {
+  return (
+    <div className="py-5 mb-10 flex flex-col gap-5 items-start">
+      <div className="flex flex-col gap-2">
+        <h2 className="font-bold text-2xl">Bot Training</h2>
+        <Separator orientation="horizontal" />
+        <p className="text-sm font-light">
+          Set FAQ questions, create questions for capturing lead information and
+          train your bot to act the way you want it to.
+        </p>
+      </div>
+      <TabsMenu triggers={HELP_DESK_TABS_MENU}>
+        <TabsContent value="help desk" className="w-full">
+          <HelpDesk id={id} />
+        </TabsContent>
+        <TabsContent value="questions" className="w-full">
+          <FilterQuestions id={id} />
+        </TabsContent>
+      </TabsMenu>
+    </div>
+  );
+};
 
 export default BotTrainingForm;

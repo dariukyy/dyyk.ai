ea161b881cd22e423a8fbdd3c838af9d3f351e1e
Darius <dpdarius1@gmail.com>
2024-09-12 23:08:20 +0300

    modifiyed accordian component for a other usage

--- src/components/accordian/index.tsx
+++ src/components/accordian/index.tsx
@@ -4,21 +4,20 @@ import {
   AccordionTrigger,
   Accordion as ShadcnAccordion,
 } from "@/components/ui/accordion";
-import { useHelpDesk } from "@/hooks/settings/use-settings";
+import { DialogClose } from "@radix-ui/react-dialog";
 import { CircleX, EditIcon } from "lucide-react";
+import EditHelpDeskQuestion from "../forms/settings/edit-help-desk-question";
 import Modal from "../modal";
 import { Button } from "../ui/button";
-import { DialogClose } from "@radix-ui/react-dialog";
-import { FieldValues, UseFormRegister } from "react-hook-form";
-import EditHelpDeskQuestion from "../forms/settings/edit-help-desk-question";
 
 type Props = {
-  id: string;
+  production: boolean;
+  id?: string;
   trigger: string;
   content: string;
-  deleteQuestion: () => void;
-  setIsOpenEditModal: (value: boolean) => void;
-  onUpdateQuestion: (id: string, question: string, answer: string) => void;
+  deleteQuestion?: () => void;
+  setIsOpenEditModal?: (value: boolean) => void;
+  onUpdateQuestion?: (id: string, question: string, answer: string) => void;
 };
 
 const Accordion = ({
@@ -27,54 +26,68 @@ const Accordion = ({
   trigger,
   deleteQuestion,
   setIsOpenEditModal,
+  production,
 
   onUpdateQuestion,
 }: Props) => {
-  return (
-    <div className="flex gap-4 justify-between items-center ">
-      <ShadcnAccordion className="flex-1" type="single" collapsible>
+  if (production)
+    return (
+      <ShadcnAccordion type="single" collapsible>
         <AccordionItem value="item-1">
           <AccordionTrigger>{trigger}</AccordionTrigger>
           <AccordionContent>{content}</AccordionContent>
         </AccordionItem>
       </ShadcnAccordion>
+    );
+  else {
+    return (
+      <div className="flex gap-4 justify-between items-center ">
+        <ShadcnAccordion className="flex-1" type="single" collapsible>
+          <AccordionItem value="item-1">
+            <AccordionTrigger>{trigger}</AccordionTrigger>
+            <AccordionContent>{content}</AccordionContent>
+          </AccordionItem>
+        </ShadcnAccordion>
 
-      <div className="flex gap-3 items-start justify-center">
-        <Modal
-          title="Edit question"
-          description=" "
-          trigger={
-            <EditIcon
-              onClick={() => setIsOpenEditModal(true)}
-              className="w-6 h-6 stroke-gray-500 cursor-pointer"
+        <div className="flex gap-3 items-start justify-center">
+          <Modal
+            title="Edit question"
+            description=" "
+            trigger={
+              <EditIcon
+                onClick={() => setIsOpenEditModal!(true)}
+                className="w-6 h-6 stroke-gray-500 cursor-pointer"
+              />
+            }
+          >
+            <EditHelpDeskQuestion
+              id={id!}
+              helpDeskQuestion={trigger}
+              helpDeskAnswer={content}
+              onUpdateQuestion={onUpdateQuestion!}
             />
-          }
-        >
-          <EditHelpDeskQuestion
-            id={id}
-            helpDeskQuestion={trigger}
-            helpDeskAnswer={content}
-            onUpdateQuestion={onUpdateQuestion}
-          />
-        </Modal>
+          </Modal>
 
-        <Modal
-          title={`Delete question: ${trigger}`}
-          description="Are you sure you want to delete this permanently? This action cannot be undone."
-          trigger={<CircleX className="w-6 h-6 text-red-400 cursor-pointer" />}
-        >
-          <div className="flex gap-3 justify-end items-center">
-            <DialogClose asChild>
-              <Button variant="ghost">Cancel</Button>
-            </DialogClose>
-            <Button onClick={deleteQuestion} variant="destructive">
-              Delete
-            </Button>
-          </div>
-        </Modal>
+          <Modal
+            title={`Delete question: ${trigger}`}
+            description="Are you sure you want to delete this permanently? This action cannot be undone."
+            trigger={
+              <CircleX className="w-6 h-6 text-red-400 cursor-pointer" />
+            }
+          >
+            <div className="flex gap-3 justify-end items-center">
+              <DialogClose asChild>
+                <Button variant="ghost">Cancel</Button>
+              </DialogClose>
+              <Button onClick={deleteQuestion} variant="destructive">
+                Delete
+              </Button>
+            </div>
+          </Modal>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default Accordion;

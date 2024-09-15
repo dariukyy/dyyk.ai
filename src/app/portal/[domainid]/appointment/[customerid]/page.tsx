59ae1ec673f8a6cc2acddb5c6bdec7546e648f6d
Darius <dpdarius1@gmail.com>
2024-09-16 01:25:54 +0300

    adding portal pages to answer questions and book appointments

---
+++ src/app/portal/[domainid]/appointment/[customerid]/page.tsx
@@ -0,0 +1,29 @@
+import {
+  onDomainCustomerResponses,
+  onGetAllDomainBookings,
+} from "@/actions/appointment";
+import PortalForm from "@/components/forms/portal/portal-form";
+import React from "react";
+
+type Props = { params: { domainid: string; customerid: string } };
+
+async function Page({ params }: Props) {
+  const questions = await onDomainCustomerResponses(params.customerid);
+  const bookings = await onGetAllDomainBookings(params.domainid);
+  console.log(questions, bookings, "DATA");
+  if (!questions) return null;
+  return (
+    <div className="pb-10">
+      <PortalForm
+        bookings={bookings}
+        email={questions.email!}
+        domainid={params.domainid}
+        customerId={params.customerid}
+        questions={questions.questions}
+        type="Appointment"
+      />
+    </div>
+  );
+}
+
+export default Page;

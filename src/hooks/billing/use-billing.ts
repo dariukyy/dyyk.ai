c85710edc98e52681a7e0f46ad4ab456d17fb2ff
Darius <dpdarius1@gmail.com>
2024-09-16 14:02:44 +0300

    adding useStripe custom hook

---
+++ src/hooks/billing/use-billing.ts
@@ -0,0 +1,24 @@
+import axios from "axios";
+import { useState } from "react";
+
+export const useStripe = () => {
+  const [onStripeAccountPending, setOnStripeAccountPending] =
+    useState<boolean>(false);
+
+  const onStripeConnect = async () => {
+    try {
+      setOnStripeAccountPending(true);
+      const account = await axios.get("/api/stripe/connect");
+      if (account) {
+        setOnStripeAccountPending(false);
+        if (account) {
+          window.location.href = account.data.url;
+        }
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return { onStripeConnect, onStripeAccountPending };
+};

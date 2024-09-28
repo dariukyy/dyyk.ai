befec718ecfa7d7056ef2ee7a878399869e97a70
Darius <dpdarius1@gmail.com>
2024-09-28 23:25:22 +0300

    configure middleware for stripe webhook

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,21 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/", "/auth(.*)", "/portal(.*)", "/images(.*)"],
+  publicRoutes: [
+    "/",
+    "/auth(.*)",
+    "/portal(.*)",
+    "/images(.*)",
+    "/api/stripe/connect",
+  ],
   ignoredRoutes: ["/chatbot"],
 });
 
 export const config = {
-  matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
+    "/(api|trpc)(.*)",
+  ],
 };

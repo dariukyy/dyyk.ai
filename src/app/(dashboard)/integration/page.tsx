e92c264c99e2b3ba8f4999d5d6366f432f58079c
Darius <dpdarius1@gmail.com>
2024-09-28 23:26:41 +0300

    v1 of stripe connect(integrate) UI

--- src/app/(dashboard)/integration/page.tsx
+++ src/app/(dashboard)/integration/page.tsx
@@ -1,9 +1,11 @@
 import { onGetPaymentConnected } from "@/actions/settings";
 import InfoBar from "@/components/infobar";
 import IntegrationsList from "@/components/integrations";
+import { currentUser } from "@clerk/nextjs";
 
 async function IntegrationsPage() {
   const payment = await onGetPaymentConnected();
+  const user = await currentUser();
 
   const connections = {
     stripe: payment ? true : false,

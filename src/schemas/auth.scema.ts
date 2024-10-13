c215d4e6621bd5b2330018422091ec51befb9bcb
Darius <dpdarius1@gmail.com>
2024-10-14 02:02:14 +0300

    fixing zod issue

--- src/schemas/auth.scema.ts
+++ src/schemas/auth.scema.ts
@@ -50,12 +50,9 @@ export type ChangePasswordProps = {
 
 export const UserLoginSchema: ZodType<UserLoginProps> = z.object({
   email: z.string().email({ message: "You did not enter a valid email" }),
-  password: z
-    .string()
-    .min(8, { message: "Your password must be atleast 8 characters long" })
-    .max(64, {
-      message: "Your password can not be longer then 64 characters long",
-    }),
+  password: z.string().max(64, {
+    message: "Your password can not be longer then 64 characters long",
+  }),
 });
 
 export const ChangePasswordSchema: ZodType<ChangePasswordProps> = z

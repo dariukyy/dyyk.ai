219e6bdbeff47865b2a0556045b07198d6cceefe
Darius <dpdarius1@gmail.com>
2024-10-14 01:55:32 +0300

    cusotm landing page for product, whitout content inside of it, only raw, but functioning

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,100 +1,34 @@
-import { onGetBlogPosts } from "@/actions/landing";
-import NavBar from "@/components/navbar";
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { pricingCards } from "@/constants/landing-page";
-import clsx from "clsx";
-import { ArrowRightCircleIcon, Check } from "lucide-react";
-import Image from "next/image";
-import Link from "next/link";
-import parse from "html-react-parser";
-import { getMonthName } from "@/lib/utils";
+import Header from "@/components/landing-page/header";
+import Hero2 from "@/components/landing-page/hero";
+import Logos from "@/components/landing-page/logos";
+import Problem from "@/components/landing-page/problem";
+import Solution from "@/components/landing-page/solution";
+import HowItWorks from "@/components/landing-page/how-it-works";
+import TestimonialsCarousel from "@/components/landing-page/testimonials-carousel";
+import Features from "@/components/landing-page/features";
+import Testimonials from "@/components/landing-page/testimonials";
+import Pricing from "@/components/landing-page/pricing";
+import FAQ from "@/components/landing-page/faq";
+import CTA from "@/components/landing-page/cta";
+import Footer from "@/components/landing-page/footer";
 
-export default async function Home() {
+export default function Home() {
   return (
-    <main>
-      <NavBar />
-      <section>
-        <div className="flex items-center justify-center flex-col mt-[80px] gap-4 ">
-          <span className="text-orange bg-orange/20 px-4 py-2 rounded-full text-sm">
-            An AI powered sales assistant chatbot
-          </span>
-          <Image
-            src="/images/corinna-ai-logo.png"
-            width={500}
-            height={100}
-            alt="Logo"
-            className="max-w-lg object-contain"
-          />
-          <p className="text-center max-w-[500px]">
-            Your AI powered sales assistant! Embed Corinna AI into any website
-            with just a snippet of code!
-          </p>
-          <Button className="bg-orange font-bold text-white px-4">
-            Start For Free
-          </Button>
-          <Image
-            src="/images/iphonecorinna.png"
-            width={400}
-            height={100}
-            alt="Logo"
-            className="max-w-lg object-contain"
-          />
-        </div>
-      </section>
-      <section className="flex justify-center items-center flex-col gap-4 mt-10">
-        <h2 className="text-4xl text-center"> Choose what fits you right</h2>
-        <p className="text-muted-foreground text-center max-w-lg">
-          Our straightforward pricing plans are tailored to meet your needs. If
-          {" you're"} not ready to commit you can get started for free.
-        </p>
-      </section>
-      <div className="flex  justify-center gap-4 flex-wrap mt-6">
-        {pricingCards.map((card) => (
-          <Card
-            key={card.title}
-            className={clsx("w-[300px] flex flex-col justify-between", {
-              "border-2 border-primary": card.title === "Unlimited",
-            })}
-          >
-            <CardHeader>
-              <CardTitle className="text-orange">{card.title}</CardTitle>
-              <CardDescription>
-                {pricingCards.find((c) => c.title === card.title)?.description}
-              </CardDescription>
-            </CardHeader>
-            <CardContent>
-              <span className="text-4xl font-bold">{card.price}</span>
-              <span className="text-muted-foreground">
-                <span>/ month</span>
-              </span>
-            </CardContent>
-            <CardFooter className="flex flex-col items-start gap-4">
-              <div>
-                {card.features.map((feature) => (
-                  <div key={feature} className="flex gap-2">
-                    <Check />
-                    <p>{feature}</p>
-                  </div>
-                ))}
-              </div>
-              <Link
-                href={`/dashbord?plan=${card.title}`}
-                className="bg-[#f3d299] border-orange border-2 p-2 w-full text-center font-bold rounded-md"
-              >
-                Get Started
-              </Link>
-            </CardFooter>
-          </Card>
-        ))}
-      </div>
+    <main className="relative">
+      <Header />
+      <Hero2 />
+      <Logos />
+      <Problem />
+      <Solution />
+      <HowItWorks />
+      <TestimonialsCarousel />
+      <Features />
+      <Testimonials />
+      <Pricing />
+      <FAQ />
+      {/* <Blog /> */}
+      <CTA />
+      <Footer />
     </main>
   );
 }

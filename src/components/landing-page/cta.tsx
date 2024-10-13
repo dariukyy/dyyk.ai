549b356cc11f474e88abfe29bd734368fc1d12e2
Darius <dpdarius1@gmail.com>
2024-10-14 02:28:35 +0300

    making ternaries for checking if user exist and based on that show content in cta button

--- src/components/landing-page/cta.tsx
+++ src/components/landing-page/cta.tsx
@@ -1,10 +1,16 @@
+"use client";
+
 import { Icons } from "./icons";
 import Link from "next/link";
 import { buttonVariants } from "../ui/button";
 import { cn } from "@/lib/utils";
 import Section from "./section";
+import { useAuth } from "@clerk/nextjs";
+import PulsatingButton from "../ui/pulsating-button";
 
 export default function CtaSection() {
+  const { userId } = useAuth();
+
   return (
     <Section
       id="cta"
@@ -13,16 +19,22 @@ export default function CtaSection() {
       className="rounded-xl bg-primary/10 py-16"
     >
       <div className="flex w-full flex-col items-center justify-center space-y-4 pt-4 sm:flex-row sm:space-x-4 sm:space-y-0">
-        <Link
-          href="/auth/sign-up"
-          className={cn(
-            buttonVariants({ variant: "default" }),
-            "flex w-full gap-2 text-background sm:w-auto"
-          )}
-        >
-          <Icons.logo className="size-6" />
-          Get started for free
-        </Link>
+        <PulsatingButton className="p-0">
+          <Link
+            href={`${
+              userId === null || undefined ? "/auth/sign-up" : "/dashboard"
+            }`}
+            className={cn(
+              buttonVariants({ variant: "default" }),
+              "flex w-full gap-2 text-background sm:w-auto"
+            )}
+          >
+            <Icons.logo className="size-6" />
+            {userId === null || undefined
+              ? "Get started for free"
+              : "Go To Dashboard"}
+          </Link>
+        </PulsatingButton>
       </div>
     </Section>
   );
